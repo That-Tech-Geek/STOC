9754dfc94e6e9466f7f349d31dbcbb3436a12f24
Sambit Mishra <labsquantumleap@gmail.com>
2024-07-05 19:01:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -40,101 +40,103 @@ def main():
     company_name = st.text_input("Enter a company ticker symbol:")
     
     if company_name:
-        data = yf.download(company_name, start='2000-01-01', end=date.today(), progress=False)
-        
-        st.write("Data since 2000-01-01 to date of usage:")
-        st.dataframe(data)
-        
-        columns = ['Close', 'Adj Close', 'Volume', 'Open']
-        column_titles = {'Close': 'Closing Stock Prices', 'Adj Close': 'Adjusted Closing Stock Prices', 'Volume': 'Share Trade Volume', 'Open': 'Opening Stock Prices'}
-        column_desc = {'Close': 'The closing price of the stock at the end of the trading day.', 
-                    'Adj Close': 'The adjusted closing price of the stock at the end of the trading day, adjusted for dividends and splits.', 
-                    'Volume': 'The number of shares traded during the day.',
-                    'Open': 'The opening price of the stock at the beginning of the trading day.'}
-        plot_descriptions = {
-            'Close': 'This graph shows the trend of closing stock prices for the selected company over the specified time period.',
-            'Adj Close': 'This graph shows the trend of adjusted closing stock prices for the selected company over the specified time period.',
-            'Volume': 'This graph shows the share trade volume for the selected company over the specified time period.',
-            'Close-Adj Close': "This graph compares the trend of closing stock prices and adjusted closing stock prices for the selected company over the specified time period. This is important so that the investor can know about the dividend payouts of the stock. Contrary to popular belief, the majority of an investor's earnings come from dividend payouts, and not the resale value of the stock he/she holds.",
-            'Close-Open': "This graph compares the trend of closing stock prices and opening stock prices for the selected company over the specified time period. This is important so that the investor can know about the daily price movement of the stock."
-        }
-        
-        option = st.selectbox('Select a plot:', ['Close', 'Adj Close', 'Volume', 'Close-Adj Close', 'Close-Open'])
-        
-        if option == 'Close':
-            st.write(f"**You selected: {column_titles[option]}**")
-            st.write(f"{column_desc[option]}")
-            st.write(f"{plot_descriptions[option]}")
-            fig, ax = plt.subplots(figsize=(10,6))
-            plt.style.use('dark_background')  # Set dark background
-            ax.spines['bottom'].set_color('white')  # Set axis color
-            ax.spines['left'].set_color('white')
-            ax.spines['top'].set_color('white')
-            ax.spines['right'].set_color('white')
-            ax.tick_params(axis='x', colors='white')
-            ax.tick_params(axis='y', colors='white')
-            ax.plot(data[option])
-            st.pyplot(fig)
-        elif option == 'Adj Close':
-            st.write(f"**You selected: {column_titles[option]}**")
-            st.write(f"{column_desc[option]}")
-            st.write(f"{plot_descriptions[option]}")
-            fig, ax = plt.subplots(figsize=(10,6))
-            plt.style.use('dark_background')  # Set dark background
-            ax.spines['bottom'].set_color('white')  # Set axis color
-            ax.spines['left'].set_color('white')
-            ax.spines['top'].set_color('white')
-            ax.spines['right'].set_color('white')
-            ax.tick_params(axis='x', colors='white')
-            ax.tick_params(axis='y', colors='white')
-            ax.plot(data[option])
-            st.pyplot(fig)
-        elif option == 'Volume':
-            st.write(f"**You selected: {column_titles[option]}**")
-            st.write(f"{column_desc[option]}")
-            st.write(f"{plot_descriptions[option]}")
-            fig, ax = plt.subplots(figsize=(10,6))
-            plt.style.use('dark_background')  # Set dark background
-            ax.spines['bottom'].set_color('white')  # Set axis color
-            ax.spines['left'].set_color('white')
-            ax.spines['top'].set_color('white')
-            ax.spines['right'].set_color('white')
-            ax.tick_params(axis='x', colors='white')
-            ax.tick_params(axis='y', colors='white')
-            ax.plot(data[option])
-            st.pyplot(fig)
-        elif option == 'Close-Adj Close':
-            st.write(f"**You selected: {column_titles[option]}**")
-            st.write(f"{column_desc[option]}")
-            st.write(f"{plot_descriptions[option]}")
-            fig, ax = plt.subplots(figsize=(10,6))
-            plt.style.use('dark_background')  # Set dark background
-            ax.spines['bottom'].set_color('white')  # Set axis color
-            ax.spines['left'].set_color('white')
-            ax.spines['top'].set_color('white')
-            ax.spines['right'].set_color('white')
-            ax.tick_params(axis='x', colors='white')
-            ax.tick_params(axis='y', colors='white')
-            ax.plot(data['Close'], label='Close')
-            ax.plot(data['Adj Close'], label='Adj Close')
-            ax.legend()
-            st.pyplot(fig)
-        elif option == 'Close-Open':
-            st.write(f"**You selected: {column_titles[option]}**")
-            st.write(f"{column_desc[option]}")
-            st.write(f"{plot_descriptions[option]}")
-            fig, ax = plt.subplots(figsize=(10,6))
-            plt.style.use('dark_background')  # Set dark background
-            ax.spines['bottom'].set_color('white')  # Set axis color
-            ax.spines['left'].set_color('white')
-            ax.spines['top'].set_color('white')
-            ax.spines['right'].set_color('white')
-            ax.tick_params(axis='x', colors='white')
-            ax.tick_params(axis='y', colors='white')
-            ax.plot(data['Close'], label='Close')
-            ax.plot(data['Open'], label='Open')
-            ax.legend()
-            st.pyplot(fig)
+        try:
+            data = yf.download(company_name + '.NS', start='2000-01-01', end=date.today(), progress=False)
+            st.write("Data since 2000-01-01 to date of usage:")
+            st.dataframe(data)
+            
+            columns = ['Close', 'Adj Close', 'Volume', 'Open']
+            column_titles = {'Close': 'Closing Stock Prices', 'Adj Close': 'Adjusted Closing Stock Prices', 'Volume': 'Share Trade Volume', 'Open': 'Opening Stock Prices'}
+            column_desc = {'Close': 'The closing price of the stock at the end of the trading day.', 
+                        'Adj Close': 'The adjusted closing price of the stock at the end of the trading day, adjusted for dividends and splits.', 
+                        'Volume': 'The number of shares traded during the day.',
+                        'Open': 'The opening price of the stock at the beginning of the trading day.'}
+            plot_descriptions = {
+                'Close': 'This graph shows the trend of closing stock prices for the selected company over the specified time period.',
+                'Adj Close': 'This graph shows the trend of adjusted closing stock prices for the selected company over the specified time period.',
+                'Volume': 'This graph shows the share trade volume for the selected company over the specified time period.',
+                'Close-Adj Close': "This graph compares the trend of closing stock prices and adjusted closing stock prices for the selected company over the specified time period. This is important so that the investor can know about the dividend payouts of the stock. Contrary to popular belief, the majority of an investor's earnings come from dividend payouts, and not the resale value of the stock he/she holds.",
+                'Close-Open': "This graph compares the trend of closing stock prices and opening stock prices for the selected company over the specified time period. This is important so that the investor can know about the daily price movement of the stock."
+            }
+            
+            option = st.selectbox('Select a plot:', ['Close', 'Adj Close', 'Volume', 'Close-Adj Close', 'Close-Open'])
+            
+            if option == 'Close':
+                st.write(f"**You selected: {column_titles[option]}**")
+                st.write(f"{column_desc[option]}")
+                st.write(f"{plot_descriptions[option]}")
+                fig, ax = plt.subplots(figsize=(10,6))
+                plt.style.use('dark_background')  # Set dark background
+                ax.spines['bottom'].set_color('white')  # Set axis color
+                ax.spines['left'].set_color('white')
+                ax.spines['top'].set_color('white')
+                ax.spines['right'].set_color('white')
+                ax.tick_params(axis='x', colors='white')
+                ax.tick_params(axis='y', colors='white')
+                ax.plot(data[option])
+                st.pyplot(fig)
+            elif option == 'Adj Close':
+                st.write(f"**You selected: {column_titles[option]}**")
+                st.write(f"{column_desc[option]}")
+                st.write(f"{plot_descriptions[option]}")
+                fig, ax = plt.subplots(figsize=(10,6))
+                plt.style.use('dark_background')  # Set dark background
+                ax.spines['bottom'].set_color('white')  # Set axis color
+                ax.spines['left'].set_color('white')
+                ax.spines['top'].set_color('white')
+                ax.spines['right'].set_color('white')
+                ax.tick_params(axis='x', colors='white')
+                ax.tick_params(axis='y', colors='white')
+                ax.plot(data[option])
+                st.pyplot(fig)
+            elif option == 'Volume':
+                st.write(f"**You selected: {column_titles[option]}**")
+                st.write(f"{column_desc[option]}")
+                st.write(f"{plot_descriptions[option]}")
+                fig, ax = plt.subplots(figsize=(10,6))
+                plt.style.use('dark_background')  # Set dark background
+                ax.spines['bottom'].set_color('white')  # Set axis color
+                ax.spines['left'].set_color('white')
+                ax.spines['top'].set_color('white')
+                ax.spines['right'].set_color('white')
+                ax.tick_params(axis='x', colors='white')
+                ax.tick_params(axis='y', colors='white')
+                ax.plot(data[option])
+                st.pyplot(fig)
+            elif option == 'Close-Adj Close':
+                st.write(f"**You selected: {column_titles[option]}**")
+                st.write(f"{column_desc[option]}")
+                st.write(f"{plot_descriptions[option]}")
+                fig, ax = plt.subplots(figsize=(10,6))
+                plt.style.use('dark_background')  # Set dark background
+                ax.spines['bottom'].set_color('white')  # Set axis color
+                ax.spines['left'].set_color('white')
+                ax.spines['top'].set_color('white')
+                ax.spines['right'].set_color('white')
+                ax.tick_params(axis='x', colors='white')
+                ax.tick_params(axis='y', colors='white')
+                ax.plot(data['Close'], label='Close')
+                ax.plot(data['Adj Close'], label='Adj Close')
+                ax.legend()
+                st.pyplot(fig)
+            elif option == 'Close-Open':
+                st.write(f"**You selected: {column_titles[option]}**")
+                st.write(f"{column_desc[option]}")
+                st.write(f"{plot_descriptions[option]}")
+                fig, ax = plt.subplots(figsize=(10,6))
+                plt.style.use('dark_background')  # Set dark background
+                ax.spines['bottom'].set_color('white')  # Set axis color
+                ax.spines['left'].set_color('white')
+                ax.spines['top'].set_color('white')
+                ax.spines['right'].set_color('white')
+                ax.tick_params(axis='x', colors='white')
+                ax.tick_params(axis='y', colors='white')
+                ax.plot(data['Close'], label='Close')
+                ax.plot(data['Open'], label='Open')
+                ax.legend()
+                st.pyplot(fig)
+        except:
+            st.write("Error: Unable to download data for the specified company. Please check the company ticker symbol and try again.")
 
 if __name__ == '__main__':
     main()
