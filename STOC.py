50e710a6de349f4074c3e234a408ea37f92f6828
Sambit Mishra <labsquantumleap@gmail.com>
2024-07-18 08:44:14 +0530

    Update STOC.py

--- STOC.py
+++ STOC.py
@@ -302,12 +302,10 @@ def main():
     # Input fields
     ticker = st.text_input("Enter stock ticker:")
     exchange = st.selectbox("Select exchange:", list(exchange_suffixes.keys()))
-    start_date = st.date_input("Start date:", value=pd.to_datetime('1924-01-01'), min_value=pd.to_datetime('1924-01-01'))
-    end_date = st.date_input("End date:", value=pd.to_datetime('today'), min_value=start_date)
+    yesterday = datetime.today() - timedelta(days=1)
+    start_date = st.date_input("Start date:", value=pd.to_datetime('1924-01-01'), min_value=pd.to_datetime('1924-01-01'), max_value=yesterday)
+    end_date = st.date_input("End date:", value=yesterday, min_value=start_date)
     
-    if ticker and exchange and start_date and end_date:
-        ticker_with_suffix = ticker + exchange_suffixes[exchange]
-        data = fetch_data(ticker_with_suffix, start=start_date, end=end_date)
     if ticker and exchange and start_date and end_date:
         ticker_with_suffix = ticker + exchange_suffixes[exchange]
         data = fetch_data(ticker_with_suffix, start=start_date, end=end_date)
