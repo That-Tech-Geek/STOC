553435a7b54a5b6f7c06c4c728ed0c31e0ac8d16
Sambit Mishra <labsquantumleap@gmail.com>
2024-07-18 07:02:02 +0530

    Update STOC.py

--- STOC.py
+++ STOC.py
@@ -471,7 +471,8 @@ if parameter_to_plot == 'VIX':
     ax.spines['left'].set_color('black')
     st.pyplot(fig)
 else:
-    if data is not None:  # Check if data is not None
+    data = yf.download(ticker, start=start_date, end=end_date, progress=False)  # Define data here
+    if not data.empty:  # Check if data is not empty
         plt.style.use('dark_background')  # Set plot background to black
         fig, ax = plt.subplots(figsize=(12, 6))
         ax.plot(data.index, data[parameter_to_plot], color='blue')  # Set plot color to blue
